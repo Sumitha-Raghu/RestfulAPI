f2c917a4dd4819d9737d29f76c63df0759829876
Sumitha <rsumitha02@gmail.com>
2023-03-19 23:27:33 -0600

    First commit

---
+++ src/main/java/GetAllIncidents.java
@@ -0,0 +1,23 @@
+import org.testng.annotations.Test;
+
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
+
+public class GetAllIncidents {
+	
+	@Test
+	public void getIncidents() {
+		RestAssured.baseURI = "https://dev75350.service-now.com/api/now/table/incident";
+		RestAssured.authentication = RestAssured.basic("admin", "nloB*^2PcRM1");
+//3. Construct the request
+		
+		RequestSpecification inputRequest = RestAssured.given();
+//		4. Send the request	
+		Response response = inputRequest.get();
+//		5. Validate the response
+		response.prettyPrint();
+
+	}
+
+}
